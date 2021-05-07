ef7fd549501d6fd2b0aa4af5f7de2a0912782e06
SaurabhToni <saurabhkumartoni@gmail.com>
2021-05-07 13:42:14 +0530

    Improvement

--- src/com/practice/FilterMovieData.java
+++ src/com/practice/FilterMovieData.java
@@ -7,26 +7,35 @@ public class FilterMovieData {
 
     public static void filterByTypeTvShow(List<Movie> movie, int n, Date startDate, Date endDate)
     {
+        long startTime = System.currentTimeMillis();
         movie.stream().filter(m -> m.getDate_added() != null)
                 .filter(m -> m.getType().equals("TV Show")).limit(n)
                 .filter(m -> m.getDate_added().after(startDate) && m.getDate_added().before(endDate))
                 .forEach(System.out::println);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken for the Process to complete is " + (endTime - startTime) + "ms");
     }
 
     public static void findListedHorrorMovie(List<Movie> movie, int n, Date startDate, Date endDate)
     {
+        long startTime = System.currentTimeMillis();
         movie.stream().filter(m -> m.getDate_added() != null)
                 .filter(m -> m.getListed_in().contains("Horror Movies")).limit(n)
                 .filter(m -> m.getDate_added().after(startDate) && m.getDate_added().before(endDate))
                 .forEach(System.out::println);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken for the Process to complete is " + (endTime - startTime) + "ms");
     }
 
     public static void filterIndianMovie(List<Movie> movie, int n, Date startDate, Date endDate)
     {
+        long startTime = System.currentTimeMillis();
         movie.stream().filter(m -> m.getDate_added() != null)
                 .filter(m -> m.getType().equals("Movie") && m.getCountry().contains("India"))
                 .limit(n)
                 .filter(m -> m.getDate_added().after(startDate) && m.getDate_added().before(endDate))
                 .forEach(System.out::println);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken for the Process to complete is " + (endTime - startTime) + "ms");
     }
 }
